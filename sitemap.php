9b4dbd8c465c9fcdc2d8a9376c78b12463b0d83a
arionas188 <arionaskonstantinos@me.com>
2025-12-16 12:52:33 +0200

    upd2 .php files upd28

--- sitemap.php
+++ sitemap.php
@@ -2,7 +2,7 @@
 header('Content-Type: application/xml; charset=utf-8');
 echo '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
 ?>
-<sitemapindex xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <sitemap>
     <loc>https://www.workroomarchitects.gr/sitemap-main.php</loc>
     <lastmod><?php echo date('Y-m-d'); ?></lastmod>
